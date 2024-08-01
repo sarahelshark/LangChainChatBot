fb772bc365bbe865ddab870647f0b9144c8e8893
sarahelshark <selsharkawy@alpenite.com>
2024-08-01 16:25:53 +0200

    add
    chain core concepts  & prompt template with chat model examples

--- learning_playground/promptPlayground.py
+++ learning_playground/promptPlayground.py
@@ -69,3 +69,48 @@ print(prompt)
 #prompt = prompt_template.invoke({"topic": "lawyers", "joke_count": 3})
 #print("\n----- Prompt with System and Human Messages (Tuple) -----\n")
 #print(prompt)
+
+#### integrate the prompt with the chat model #######################################################################################
+
+# PART 1: Create a ChatPromptTemplate using a template string
+print("-----Prompt from Template-----")
+template = "Tell me a joke about {topic}."
+prompt_template = ChatPromptTemplate.from_template(template)
+
+prompt = prompt_template.invoke({"topic": "cats"})
+
+# Access the content of the human message in the prompt  >> se voglio 'pulire' il prompt e mi serve solo il contenuto
+# human_message_content = None
+# for message in prompt.messages:
+#     if isinstance(message, HumanMessage):
+#         human_message_content = message.content
+#         break
+# Print the human message content
+# print("Human message content:", human_message_content)
+
+result = model.invoke(prompt)
+print(prompt, " "," Model reply :", " ", result.content)
+
+# PART 2: Prompt with Multiple Placeholders
+print("\n----- Prompt with Multiple Placeholders -----\n")
+template_multiple = """You are a helpful assistant.
+Human: Tell me a {adjective} short story about a {animal}.
+Assistant:"""
+prompt_multiple = ChatPromptTemplate.from_template(template_multiple)
+prompt = prompt_multiple.invoke({"adjective": "funny", "animal": "panda"})
+
+result = model.invoke(prompt)
+print(result.content)
+
+# PART 3: Prompt with System and Human Messages (Using Tuples)
+print("\n----- Prompt with System and Human Messages (Tuple) -----\n")
+messages = [
+    ("system", "You are a comedian who tells jokes about {topic}."),
+    ("human", "Tell me {joke_count} jokes."),
+]
+prompt_template = ChatPromptTemplate.from_messages(messages)
+prompt = prompt_template.invoke({"topic": "lawyers", "joke_count": 3})
+result = model.invoke(prompt)
+print(result.content)
+
+
