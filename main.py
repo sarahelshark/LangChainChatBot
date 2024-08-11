aa5ec3aad83daf7c91e605d225c52ec0e21cc2f5
Daniel Mutu <dmutu@alpenite.com>
2024-08-11 02:31:29 +0200

    quickfix

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from flask_bootstrap import Bootstrap
 from flask_cors import CORS
 
 import os
-from dotenv import load_dotenv 
 import json
+import logging
+from dotenv import load_dotenv 
 from datetime import datetime
 
 import constants
@@ -15,20 +16,14 @@ from langchain_openai import ChatOpenAI
 from langchain.schema import AIMessage, HumanMessage, SystemMessage
 load_dotenv()
 
-
-#imports for vectorization
-from langchain.text_splitter import CharacterTextSplitter
-from langchain_community.document_loaders import TextLoader
-from langchain_community.vectorstores import FAISS
-from langchain_openai import OpenAIEmbeddings
-
-
 # Initialize Flask app
 app = Flask(__name__)
 Bootstrap(app)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 CORS(app)
 
+# Set up Gemini credentials
+os.environ[constants.googleApplicationCredentials] = constants.alpeniteVertexai
 
 # Initialize the model
 model = ChatOpenAI(
@@ -42,7 +37,6 @@ chatgpt_history = []
 system_message = SystemMessage(content="You are a helpful AI assistant")
 chatgpt_history.append(system_message)
 
-
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -51,7 +45,6 @@ def index():
 def docs():
     return render_template('docs.html')
 
-    
 @app.route('/api/chat', methods=['POST'])
 def chat():
     global chatgpt_history
@@ -64,34 +57,30 @@ def chat():
         user_message = data.get('message', '')
         model_choice = data.get('model', 'chatgpt')  # Default to ChatGPT if not specified  
         
-        
         if user_message.lower() == 'exit':
-            print("---- Message History ----")
-            print(chatgpt_history)
+            logging.info("---- Message History ----")
+            logging.info(chatgpt_history)
             # Serialize each message in the history & convert to JSON
             serialized_history = [serialize_message(msg) for msg in chatgpt_history]
             json_gpt_data = json.dumps(serialized_history) 
-            print('chatGPT conversation history converted to json', json_gpt_data)
-            
+            logging.info('chatGPT conversation history converted to json', json_gpt_data)
             
             return jsonify({'content': "Grazie per aver utilizzato l'assistente AI. Arrivederci!👋"})
    
-                
         if model_choice == 'chatgpt':
-            print("---- ChatGPT mode ----")
+            logging.info("---- ChatGPT mode ----")
             # ChatGPT logic
             # Add user message to chat history
             chatgpt_history.append(HumanMessage(content=user_message))
             # Get AI response using history
-            result = model.invoke(chatgpt_history)
+            result = model.predict(chatgpt_history)  # Assicurati che `predict` sia il metodo corretto
             response = result.content
             # Add AI message to chat history
             chatgpt_history.append(AIMessage(content=response))
             
-            
         elif model_choice == 'gemini':
             # Gemini logic
-            print("---- Gemini mode ----")
+            logging.info("---- Gemini mode ----")
             response = GeminiPro.get_response(user_message)
         else:
             return jsonify({'error': 'Invalid model choice.'}), 400
@@ -99,6 +88,7 @@ def chat():
         return jsonify({'content': response})
     
     except Exception as e:
+        logging.error(f'Unexpected error: {str(e)}')
         return jsonify({'error': f'Unexpected error: {str(e)}'}), 500
     
 # Helper function to serialize messages
@@ -108,7 +98,6 @@ def serialize_message(message):
         'content': message.content
     }
 
-
 @app.route('/api/reset', methods=['POST'])
 def reset_conversation():
     global chatgpt_history
@@ -116,9 +105,8 @@ def reset_conversation():
         chatgpt_history = [system_message]
         return jsonify({'status': 'Conversation reset successfully.'})
     except Exception as e:
+        logging.error(f'Unexpected error: {str(e)}')
         return jsonify({'error': f'Unexpected error: {str(e)}'}), 500
 
 if __name__ == '__main__':
-    # Set up Gemini credentials
-    os.environ[constants.googleApplicationCredentials] = constants.alpeniteVertexai
     app.run(debug=True)
