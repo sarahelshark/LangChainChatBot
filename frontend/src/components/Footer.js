a0b43a3e8c359526dc5d1adc71feb7d9c416b9c4
sarahelshark <selsharkawy@alpenite.com>
2024-09-25 15:00:34 +0200

    fix footer  padding

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Footer = () => (
-    <footer className="bg-gray-200 dark:bg-gray-800 text-center p-4 mt-auto">
+    <footer className="bg-gray-200 dark:bg-gray-800 text-center p-1 mt-auto">
     <p className="text-gray-600 dark:text-gray-400">&copy; 2024 Alpenite test. All rights reserved.</p>
   </footer>
 );
