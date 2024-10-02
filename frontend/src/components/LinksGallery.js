cad5108c8399643c3c6b1be3e01f874e2f62a739
sarahelshark <selsharkawy@alpenite.com>
2024-10-02 10:44:16 +0200

    add initial content to placeholders

--- frontend/src/components/LinksGallery.js
+++ frontend/src/components/LinksGallery.js
@@ -1,7 +1,18 @@
 function LinksGallery () {
     console.log('hey');
     return(
-        <div>hey</div>
+
+       <section id="siteReference" className="mt-4 text-center">
+
+        <h4> YOU MAY ALSO NEED</h4>
+
+        <div className=" mt-4 flex flex-row space-x-3 justify-center">
+          <div className="p-4 bg-gray-100 dark:bg-gray-700 rounded">Langchain</div>
+          <div className="p-4 bg-gray-100 dark:bg-gray-700 rounded">VertexAi</div>
+          <div className="p-4 bg-gray-100 dark:bg-gray-700 rounded">Alpenite</div>
+       </div>
+       
+       </section>
     );
 }
 
--- frontend/src/components/SelectDocsType.js
+++ frontend/src/components/SelectDocsType.js
@@ -1,6 +1,21 @@
 function SelectDocsType(){
     return(
-        <div>placeholder</div>
+        <>
+        <div className="mb-4">
+         <label htmlFor="modelSelect" className="block mb-2 text-sm font-medium text-gray-700 dark:text-gray-300">
+           Choose Docs type:
+         </label>
+         <select
+           id="modelSelect"
+           value=""
+           onChange=""
+           className="p-2 border border-gray-300 rounded dark:border-gray-600 bg-white dark:bg-gray-800 text-black dark:text-white"
+         >
+           <option value="">Dev</option>
+           <option value="">User</option>
+         </select>
+       </div>
+        </>
     );
 };
 export default SelectDocsType;
