6e305da2c59418f8674ef246a6b0df0f44398d85
sarahelshark <selsharkawy@alpenite.com>
2024-10-02 11:13:31 +0200

    add hover  to links in light mode

--- frontend/src/components/LinksGallery.js
+++ frontend/src/components/LinksGallery.js
@@ -7,9 +7,9 @@ function LinksGallery () {
         <h4> YOU MAY ALSO NEED</h4>
 
         <div className=" mt-4 flex flex-row space-x-3 justify-center">
-          <div className="p-4 bg-gray-200 dark:bg-gray-700 rounded">Langchain</div>
-          <div className="p-4 bg-gray-200 dark:bg-gray-700 rounded">VertexAi</div>
-          <div className="p-4 bg-gray-200 dark:bg-gray-700 rounded">Alpenite</div>
+          <div className="p-4 bg-gray-200 rounded dark:bg-gray-700 cursor-pointer hover:bg-gray-400 hover:text-white">Langchain</div>
+          <div className="p-4 bg-gray-200 rounded dark:bg-gray-700 cursor-pointer hover:bg-gray-400 hover:text-white">VertexAi</div>
+          <div className="p-4 bg-gray-200 rounded dark:bg-gray-700 cursor-pointer hover:bg-gray-400 hover:text-white">Alpenite</div>
        </div>
        
        </section>
