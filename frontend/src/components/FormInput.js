5864bb45f93db413c8737179c5eaf9318323181f
sarahelshark <selsharkawy@alpenite.com>
2024-10-09 12:13:21 +0200

    fix mobile view of input buttons

--- frontend/src/components/FormInput.js
+++ frontend/src/components/FormInput.js
@@ -5,7 +5,7 @@ import UploadButton from './UploadButton';
 
 const FormInput = ({ input, setInput, messages, setMessages, setLoading, model, resetConversation }) => {
   return (
-    <div className="mt-4 flex">
+    <div className="mt-4 flex flex-col space-y-3 sm:flex-row sm:space-y-0 sm:space-x-3 justify-center">
       <label htmlFor="formInput" className="sr-only">
         User input
       </label>
@@ -19,9 +19,13 @@ const FormInput = ({ input, setInput, messages, setMessages, setLoading, model, 
         onChange={(e) => setInput(e.target.value)}
         placeholder="Write a message..."
       />
-      <SendButton input={input} messages={messages} setMessages={setMessages} setInput={setInput} setLoading={setLoading} model={model} />
-      <ResetButton resetConversation={resetConversation} />
-      <UploadButton input={input} messages={messages} setMessages={setMessages} setInput={setInput} />
+      <div className='mt-2 flex flex-row justify-center'>
+       <SendButton input={input} messages={messages} setMessages={setMessages} setInput={setInput} setLoading={setLoading} model={model} />
+       <ResetButton resetConversation={resetConversation} />
+       <UploadButton input={input} messages={messages} setMessages={setMessages} setInput={setInput} />    
+      </div>
+      
+      
     </div>
   );
 };
